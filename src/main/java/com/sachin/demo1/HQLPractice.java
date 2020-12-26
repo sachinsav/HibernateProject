e99d20e5537d096259ff189db2e6eaa54818a2cd
sachinsav <sachin.saw.13@gmai.com>
2020-12-26 21:19:25 +0530

    HQL-3 native SQLQuery added

--- src/main/java/com/sachin/demo1/HQLPractice.java
+++ src/main/java/com/sachin/demo1/HQLPractice.java
@@ -1,8 +1,11 @@
 package com.sachin.demo1;
 
 import java.util.List;
+import java.util.Map;
 
+import org.hibernate.Criteria;
 import org.hibernate.Query;
+import org.hibernate.SQLQuery;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -18,10 +21,29 @@ public class HQLPractice {
 		Session session = sf.openSession();
 		session.beginTransaction();
 		
-		Query q1 = session.createQuery("select id,name from Classes");
-		List<Object[]> rs = q1.list();
-		for(Object[] cl: rs) {
-			System.out.println(cl[0]+" : "+cl[1]);
+		SQLQuery q1 = session.createSQLQuery("select * from classes");
+		q1.addEntity(Classes.class);
+		List<Classes> lst = q1.list();
+		for(Classes cl:lst)
+		{
+			System.out.println(cl);
+		}
+		
+		
+		SQLQuery q2 = session.createSQLQuery("select name from classes");
+		List lst2 = q2.list();
+		for(Object cl:lst2)
+		{
+			System.out.println(cl);
+		}
+		
+		SQLQuery q3 = session.createSQLQuery("select * from classes");
+		q3.setResultTransformer(Criteria.ALIAS_TO_ENTITY_MAP);
+		List lst3 = q3.list();
+		for(Object cl:lst3)
+		{
+			Map m = (Map)cl;
+			System.out.println(m.get("id")+" : "+m.get("name"));
 		}
 		session.getTransaction().commit();
 		
